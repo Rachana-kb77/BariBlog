7ff4338a8cacf810e7340f84cef4b2647c8751cf
Rachana-kb77 <159272800+Rachana-kb77@users.noreply.github.com>
2025-05-29 00:13:27 +0530

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
